f7ebbf1a226e6334763cafd0d30416e28e0ae0da
TechTalkerShubh <jitiya66@gmail.com>
2022-07-05 23:48:20 +0530

    Slider, home implemented

--- eshop/routes/web.php
+++ eshop/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/', [FrontendController::class, 'index']);
 
 Auth::routes();
 
